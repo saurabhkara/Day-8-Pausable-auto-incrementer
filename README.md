2cd09da7f267ab73ceee826953f3ffa09940c9b5
Saurabh <saurabkumar679@gmail..com>
2023-11-09 07:51:08 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+"# Day-8-Pausable-auto-incrementer"
+
+Create a pausable auto incrementer in JavaScript, which takes an initial
+value and steps as input and increments the initial value with given steps
+every second. The incrementer can be paused and resumed back.
---
+++ index.js
@@ -0,0 +1,30 @@
+console.log("working");
+
+function autoIncrementer(intialValue = 0, steps = 10) {
+  let value = intialValue;
+  let flag = null;
+  let setIntervalId;
+  const increment = () => {
+    if (flag === null) {
+      setIntervalId = setInterval(() => {
+        value += steps;
+        console.log(value);
+      }, 1000);
+    }
+  };
+
+  const pause = () => {
+    clearInterval(setIntervalId);
+  };
+  return {
+    increment,
+    pause,
+  };
+}
+
+const options = autoIncrementer(0, 10);
+options.increment();
+
+setTimeout(() => {
+  options.pause();
+}, 6000);
